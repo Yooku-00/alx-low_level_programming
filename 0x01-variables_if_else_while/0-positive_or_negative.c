c0a94d44c3064a33975c7e773b42d2b44a3a9964
Aubrey Effah Amankwah <62848616+Yooku-00@users.noreply.github.com>
2022-12-09 21:15:17 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-	* main - main function to generate a random number
-	* Return: Always 0 (Success)
-	*/
+ * main - main function to generate a random number
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
